5579d8e34fe5ada0dd80d612faca89cb63c0d062
Suyn <594154545@qq.com>
2025-03-12 19:01:15 +0800

    Delete Unity Project directory

--- Unity Project/AlphaZero/Assets/Scripts/Connect4/Connect4Action.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Connect4Action : MonoBehaviour {
-
-    [HideInInspector] public int action; // 由Connect4Game初始化
-}
